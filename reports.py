acb7f0fb10d8e776a740467c69a6094b2c92bf13
Stefan <stefan-tiger@gmx.de>
2026-02-14 20:12:41 +0100

    added debug output in log

--- reports.py
+++ reports.py
@@ -3,6 +3,9 @@ import json
 from collections import defaultdict
 from pathlib import Path
 from db import get_connection
+import logging
+
+logger = logging.getLogger(__name__)
 
 def history_closure(conn, version_id):
     sql = """
@@ -15,7 +18,9 @@ def history_closure(conn, version_id):
     )
     SELECT id FROM history;
     """
-    return {r["id"] for r in conn.execute(sql, (version_id,))}
+    rows = [r["id"] for r in conn.execute(sql, (version_id,))]
+    logger.debug("history_closure: start=%s count=%d nodes=%s", version_id, len(rows), rows)
+    return set(rows)
 
 def active_bugs(conn, version_id):
     history = history_closure(conn, version_id)
@@ -34,10 +39,18 @@ def active_bugs(conn, version_id):
                     "title": row["title"],
                     "description": row["description"]
                 }
+                logger.debug("active_bugs: version=%s introduced_bug=%s", version_id, row["id"])
             elif row["type"] == "bugfix" and row["fixes_ticket_id"]:
                 fixes.add(row["fixes_ticket_id"])
+                logger.debug("active_bugs: version=%s bugfix=%s neutralises=%s", version_id, row["id"], row["fixes_ticket_id"])
 
-    return [b for i, b in introduced.items() if i not in fixes]
+    neutralised = [i for i in introduced.keys() if i in fixes]
+    if neutralised:
+        logger.info("active_bugs: version=%s neutralised_bugs=%s", version_id, neutralised)
+
+    active = [b for i, b in introduced.items() if i not in fixes]
+    logger.debug("active_bugs: version=%s active_count=%d active_ids=%s", version_id, len(active), [b["id"] for b in active])
+    return active
 
 def containment_tree(conn, root):
     sql = """
@@ -51,6 +64,7 @@ def containment_tree(conn, root):
     SELECT id, parent FROM deps;
     """
     nodes = list(conn.execute(sql, (root,)))
+    logger.debug("containment_tree: root=%s nodes_found=%d", root, len(nodes))
     children = defaultdict(list)
     for n in nodes:
         if n["parent"]:
@@ -62,12 +76,14 @@ def build_tree(root):
     children = containment_tree(conn, root)
 
     def build(node):
+        logger.debug("build: entering node=%s children=%s", node, children.get(node, []))
         bugs = active_bugs(conn, node)
         subnodes = [build(c) for c in children.get(node, [])]
         summary = {
             "elements": 1 + sum(s["summary"]["elements"] for s in subnodes),
             "bugs": len(bugs) + sum(s["summary"]["bugs"] for s in subnodes)
         }
+        logger.info("build: node=%s elements=%d bugs=%d", node, summary["elements"], summary["bugs"])
         return {
             "version": node,
             "active_bugs": bugs,
